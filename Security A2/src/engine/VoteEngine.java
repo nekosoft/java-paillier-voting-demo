50b9df1c5724ecf2f96732f9eb705a0c2d723d56
Yianna Paris <yiannaparis@gmail.com>
2018-05-11 17:47:57 +1000

    added test code for unicode and gcd method

--- Security A2/src/engine/VoteEngine.java
+++ Security A2/src/engine/VoteEngine.java
@@ -4,6 +4,10 @@ public class VoteEngine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("This is the Greek letter for lambda: \u03bb");
+		System.out.println("This is the Greek letter for miu: \u03bc");
+		
+		System.out.println(PrivKeyGeneration.gcd(132, 84));
 
 	}
 

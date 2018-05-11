70b539e38dbd6ea524cdceffb162f0d0e65e0f7d
Yianna Paris <yiannaparis@gmail.com>
2018-05-11 17:59:14 +1000

    test print for gcd and lcm

--- Security A2/src/engine/VoteEngine.java
+++ Security A2/src/engine/VoteEngine.java
@@ -7,7 +7,13 @@ public class VoteEngine {
 		System.out.println("This is the Greek letter for lambda: \u03bb");
 		System.out.println("This is the Greek letter for miu: \u03bc");
 		
-		System.out.println(PrivKeyGeneration.gcd(132, 84));
+		int gcdTest;
+		int lcmTest;
+		
+		gcdTest = PrivKeyGeneration.gcd(132, 84);
+		lcmTest = PrivKeyGeneration.lcm(132, 84, gcdTest);
+		
+		System.out.printf("%d %d", gcdTest, lcmTest);
 
 	}
 

87fb69610b71b8ba5ea56463d8cd200d81e58233
Yianna Paris <yiannaparis@gmail.com>
2018-05-11 17:45:31 +1000

    implemented gcd method

--- Security A2/src/engine/PrivKeyGeneration.java
+++ Security A2/src/engine/PrivKeyGeneration.java
@@ -2,6 +2,39 @@ package engine;
 
 public class PrivKeyGeneration {
 	
+	int p;
+	int q;
+	int g;
+	int n;
+	static int lcm;
+	static int gcd;
+	
+	// need public key parameters to generate private key
+	PublicKeyGeneration publicKey = new PublicKeyGeneration(p, q, g, n);
+	
+	
+	// calculates the Greatest Common Divisor using Euclid's Algorithm
+	public static int gcd(int p, int q)
+	{
+		while (p !=q) 
+		{
+			int c;
+			c = p - q;;
+			p = Math.max(q, c);
+			q = Math.min(q, c);
+			gcd = p;
+		}
+		
+		return gcd;
+	}
+	
+	public static int lcm(int p, int q, int gcd)
+	{
+		lcm = 
+		return lcm;
+	}
+	
+
 	
 
 }
